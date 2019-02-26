f9ad38a95c44ef6d65d46bf14675744501b9e893
Maël Baron <mael.baron@laposte.net>
2019-02-26 15:26:28 +0100

    add interface ProduitDAO

--- src/Modele/ProduitFactoryDAO.java
+++ src/Modele/ProduitFactoryDAO.java
@@ -1,4 +1,14 @@
 package Modele;
 
 public class ProduitFactoryDAO {
+
+    public static void createProduitDAO_Oracle(){
+        //TODO: create produit DAO
+        new ProduitDAO_Oracle();
+    }
+
+    public static void createProduitDAO_XML(){
+        new modele.ProduitDAO_XML();
+    }
+
 }
