cbe32139536b6505456d55e26ac0ac2994a7d29b
Maël Baron <mael.baron@laposte.net>
2019-02-26 15:56:12 +0100

    maj produitFacoryDAO

--- src/Modele/ProduitFactoryDAO.java
+++ src/Modele/ProduitFactoryDAO.java
@@ -3,7 +3,6 @@ package Modele;
 public class ProduitFactoryDAO {
 
     public static I_ProduitDAO createProduitDAO_Oracle(){
-        //TODO: create produit DAO
         return new ProduitDAO_Oracle();
     }
 
